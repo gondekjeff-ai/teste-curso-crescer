04242ef8e27ec26da900581124413656364c8907
gondekjeff-ai <gondek.jeff@gmail.com>
2026-04-18 01:03:27 +0000

    Deploying to stable-website from @ gondekjeff-ai/teste-curso-crescer@e79937b0fffc76f6090af4430c6bdf4499ad4a8f 🚀

--- supabase/migrations/20260316165758_50f1fb78-3bec-43eb-8635-340d234561f9.sql
+++
@@ -1,3 +0,0 @@
-
-CREATE POLICY "Allow inserts on rate_limits" ON public.rate_limits FOR INSERT WITH CHECK (true);
-CREATE POLICY "Allow reads on rate_limits" ON public.rate_limits FOR SELECT USING (true);
