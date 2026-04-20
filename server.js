041552bea8bbdfbc548232e68fae8b58ab46767a
gondekjeff-ai <gondek.jeff@gmail.com>
2026-04-20 20:53:28 +0000

    Deploying to stable-website from @ gondekjeff-ai/teste-curso-crescer@ddc96590215419ce451c99730e09c207f96a448c 🚀

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 21002;
+const PORT = process.env.PORT || 3000;
 
 // Security
 app.disable('x-powered-by');
